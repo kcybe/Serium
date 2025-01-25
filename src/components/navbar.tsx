b1334d75d404b7ae4cb3c3673b126960af94d64d
kcybe <kcybe@github.com>
2025-01-26 00:02:19 +0200

    Fixed the navbar logo disappearing after resizing the window to a small size

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,17 +11,13 @@ export function Navbar() {
   return (
     <div className="sticky top-4 z-50 flex justify-center">
       <nav className="border rounded-lg bg-background/80 backdrop-blur supports-[backdrop-filter]:bg-background/60 w-full max-w-7xl mx-4 p-2">
-        <div className="container flex items-center">
-          <div className="mr-4 hidden md:flex">
-            <Link href="/" className="mr-6 flex items-center space-x-2">
-              <AnimatedLogo />
-            </Link>
-          </div>
-          <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
-            <div className="w-full flex justify-end space-x-2">
-              <InventoryButton />
-              <SettingsButton />
-            </div>
+        <div className="container flex items-center justify-between">
+          <Link href="/" className="flex items-center space-x-2">
+            <AnimatedLogo />
+          </Link>
+          <div className="flex space-x-2">
+            <InventoryButton />
+            <SettingsButton />
           </div>
         </div>
       </nav>
