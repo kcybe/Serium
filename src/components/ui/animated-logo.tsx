19bb14cff3564f38e5791eadcf1ea0236a4187c3
kcybe <noamyusufov@gmail.com>
2025-02-03 15:21:29 +0200

    Removed the logo glitch animation

--- src/components/ui/animated-logo.tsx
+++ src/components/ui/animated-logo.tsx
@@ -32,7 +32,6 @@ export function AnimatedLogo() {
           ${pixelifySans.className} 
           text-xl 
           font-bold 
-          matrix-text
           relative
           [animation:matrix-reveal_1s_cubic-bezier(0.25,0.46,0.45,0.94)_both]
         `}
