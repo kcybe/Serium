e92058a99a206ae1720fe0fbce2d6facc284a0ca
kcybe <noamyusufov@gmail.com>
2025-03-01 15:44:00 +0200

    feat(navbar): remove settings button component as part of navbar refactor

--- src/components/navbar/components/settings-button.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { SettingsDialog } from '../../settings/dialogs/settings-dialog'
-
-export default function SettingsButton() {
-  return (
-    <SettingsDialog />
-  )
-}
